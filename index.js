6fe5665f2750424fd7b6855fd7ae9205199acd27
Castillo Ignacio <castilloignacio1510@gmail.com>
2024-12-02 13:15:37 -0300

    arreglando problema de CORS

--- index.js
+++ index.js
@@ -1,45 +1,44 @@
 import { createServer } from "node:http";
 import { neon } from "@neondatabase/serverless";
 
-const sql = neon('postgresql://Programs-db_owner:gt7QxJLNI4rP@ep-shrill-brook-a5j8odhx.us-east-2.aws.neon.tech/Programs-db?sslmode=require')
+const sql = neon(
+  "postgresql://Programs-db_owner:gt7QxJLNI4rP@ep-shrill-brook-a5j8odhx.us-east-2.aws.neon.tech/Programs-db?sslmode=require"
+);
 
 const PORT = process.env.port || 1234;
 
-
 const server = createServer(async (req, res) => {
   console.log(`Acabo de recibir una petición, a la ruta ${req.url}`);
-  
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+
   const URL = req.url;
 
   switch (URL) {
     case "/":
-      res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+      res.setHeader("Content-Type", "text/plain; charset=utf-8");
       res.end("¡Bienvenido al servidor de Soft Share!");
       break;
 
-    case "/get":
+    case "/get": // Cuando se quiera obtener todos los programas se utiliza get
       try {
-        const sql = neon(
-          "postgresql://Programs-db_owner:gt7QxJLNI4rP@ep-shrill-brook-a5j8odhx.us-east-2.aws.neon.tech/Programs-db?sslmode=require"
-        );
-
-        // Obtener datos de la base de datos
-        const result = await sql`SELECT * FROM programas`;
+        const result = await sql`SELECT * FROM programas
+    ORDER BY nombre ASC`;
 
         // Configurar la respuesta
-        res.setHeader('Content-Type', 'application/json; charset=utf-8');
+        res.setHeader("Content-Type", "application/json; charset=utf-8");
         res.end(JSON.stringify(result));
       } catch (error) {
         console.error("Error al consultar la base de datos:", error);
         res.statusCode = 500;
-        res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+        res.setHeader("Content-Type", "text/plain; charset=utf-8");
         res.end("Error al obtener los datos de la base de datos.");
       }
       break;
 
     default:
       res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+      res.setHeader("Content-Type", "text/plain; charset=utf-8");
       res.end("404 Not Found");
       break;
   }
