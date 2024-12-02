0fa1dfe0a2cef36b956dcf3d8050ef80b34b5c04
Castillo Ignacio <castilloignacio1510@gmail.com>
2024-12-02 12:43:19 -0300

    backend sencillo para traer datos

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
-import http from 'node:http';
+import { createServer } from "node:http";
+import { neon } from "@neondatabase/serverless";
 
-const PORT = process.env.port || 1234
+const sql = neon('postgresql://Programs-db_owner:gt7QxJLNI4rP@ep-shrill-brook-a5j8odhx.us-east-2.aws.neon.tech/Programs-db?sslmode=require')
 
-async function manejarSolicitudes() {
+const PORT = process.env.port || 1234;
+
+
+const server = createServer(async (req, res) => {
+  console.log(`Acabo de recibir una petición, a la ruta ${req.url}`);
   
-}
+  const URL = req.url;
 
+  switch (URL) {
+    case "/":
+      res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+      res.end("¡Bienvenido al servidor de Soft Share!");
+      break;
 
-const server = http.createServer(manejarSolicitudes)
+    case "/get":
+      try {
+        const sql = neon(
+          "postgresql://Programs-db_owner:gt7QxJLNI4rP@ep-shrill-brook-a5j8odhx.us-east-2.aws.neon.tech/Programs-db?sslmode=require"
+        );
+
+        // Obtener datos de la base de datos
+        const result = await sql`SELECT * FROM programas`;
+
+        // Configurar la respuesta
+        res.setHeader('Content-Type', 'application/json; charset=utf-8');
+        res.end(JSON.stringify(result));
+      } catch (error) {
+        console.error("Error al consultar la base de datos:", error);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+        res.end("Error al obtener los datos de la base de datos.");
+      }
+      break;
+
+    default:
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+      res.end("404 Not Found");
+      break;
+  }
+});
 
 // Creear el servidor
 // Ponerlo en escucha
 // Administrar las peticiones
+
+server.listen(PORT, () => {
+  console.log(
+    `El servidor esta en escucha por el puerto: http://localhost:${PORT}`
+  );
+});
